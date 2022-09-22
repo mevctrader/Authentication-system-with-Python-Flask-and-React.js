f0fbc95048200845a315f9c65f2239bc895807f3
mevctrader <mevctrader@gmail.com>
2022-09-22 00:16:45 +0000

    tarea para entrega

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const { store, actions } = useContext(Context);
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+        </Link>
+        <div className="ml-auto">
+          {store.user && store.user.email}
+          {"  "}
+          {!store.token ? (
+            <Link to="/login">
+              <button className="btn btn-primary">Login</button>
+            </Link>
+          ) : (
+            <button
+              onClick={() => actions.logout()}
+              className="btn btn-primary"
+            >
+              Cerrar Sesión
+            </button>
+          )}{" "}
+          {!store.token ? (
+            <Link to="/signup">
+              <button className="btn btn-primary">Registrarse</button>
+            </Link>
+          ) : null}
+        </div>
+      </div>
+    </nav>
+  );
 };
